6f1e2295b7af3c519dc84c2949aa331f9ac529dc
Trevor Renshaw <trenshaw@instructure.com>
2016-08-18 13:49:34 -0600

    Add files

---
+++ Starter Project/QuackyDucks/Assets/Scripts/TextRenderer.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class TextRenderer : MonoBehaviour {
+	
+	void Start () 
+	{
+		//A magical hack to allow a 3D text object to function within Unity 2Ds sorting layers
+		renderer.sortingLayerName = "Foreground";
+		renderer.sortingOrder = 1;	
+	}
+}
